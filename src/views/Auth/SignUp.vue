aa9f931858cc35596859b4d8fbf4d2cd55dea5c7
naimur-abanto <naimur@nerddevs.com>
2022-06-15 18:27:49 +0600

    changed some positioning in signup page

--- src/views/Auth/SignUp.vue
+++ src/views/Auth/SignUp.vue
@@ -1,35 +1,40 @@
-<template>    
-    <div> 
-        <h3>Sign up</h3>
-        <card-sign></card-sign>  
-    </div>    
-    <div class="after-buttons">
-        Have an account already?<router-link to="LogIn">Log In</router-link>
-    </div>        
+<template>
+  <div>
+    <h3>Sign up</h3>
+    <card-sign></card-sign>
+  </div>
+  <div class="after-buttons">
+    Have an account already?<router-link to="LogIn">Log In</router-link>
+  </div>
+  <the-footer class="foo"></the-footer>
 </template>
 
 <script lang="ts">
+import CardSign from "@/components/CardSign.vue";
+import TheFooter from "@/components/TheFooter.vue";
 
-import CardSign from '@/components/CardSign.vue'
-
-export default{
-    components: {
-        CardSign
-    }
-}
+export default {
+  components: {
+    CardSign,
+    TheFooter,
+  },
+};
 </script>
 
 <style scoped>
-    a{
-        list-style: none;
-        text-decoration: none;
-        color: #fe4642;;
-        font-weight: bold;
-    }    
-    .after-buttons{
-        text-align: center;
-    }
-    h3{
-        text-align: center;
-    }
+.foo {
+  transform: translateY(190px) !important;
+}
+a {
+  list-style: none;
+  text-decoration: none;
+  color: #fe4642;
+  font-weight: bold;
+}
+.after-buttons {
+  text-align: center;
+}
+h3 {
+  text-align: center;
+}
 </style>
