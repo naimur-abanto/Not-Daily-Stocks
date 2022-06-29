f7d0a84514f8348bbf10011e2f08f3ca7998832f
naimur-abanto <naimur@nerddevs.com>
2022-06-29 14:43:20 +0600

    style changes for userview

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -63,11 +63,11 @@
         </div>
 
         <div class="flex py-4">
-          <div>
+          <div class="flex-1">
             <p class="text-sm font-bold">Password</p>
             <p class="text-sm text-gray-900">{{ currentUser.password }}</p>
           </div>
-          <div class="translate-x-20 w-max">
+          <div class="">
             <button
               type="button"
               class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
